ee083056295690700537ffff0cd3c9258cd7eae9
1992kim <rladydrbs92@naver.com>
2017-08-14 20:25:17 +0900

    초기 파일

---
+++ TabLayoutExample/app/src/main/java/org/androidtown/tablayoutexample/Tab1.java
@@ -0,0 +1,21 @@
+package org.androidtown.tablayoutexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kim on 2017-08-13.
+ */
+
+public class Tab1 extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab1, container, false);
+    }
+}
