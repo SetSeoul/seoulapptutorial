160a81a394f2f503d58c859a1f8288a861745e2d
msJang <ssh://git@git.flexion.co.kr:30001/msjang/DHL-Calculator.git>
2017-09-03 01:17:22 +0900

    no message

--- TabLayoutExample/app/src/main/java/org/androidtown/tablayoutexample/MainActivity.java
+++ TabLayoutExample/app/src/main/java/org/androidtown/tablayoutexample/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-// 용
+//용균용균용균
 public class MainActivity extends AppCompatActivity {
 
     /**
