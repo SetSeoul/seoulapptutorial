6ce1b57f0174a684b0f3c9f13c25dac943736f32
msJang <ssh://git@git.flexion.co.kr:30001/msjang/DHL-Calculator.git>
2017-09-03 00:04:04 +0900

    splash change

--- TabLayoutExample/app/src/main/java/org/androidtown/tablayoutexample/MapsActivity.java
+++ TabLayoutExample/app/src/main/java/org/androidtown/tablayoutexample/MapsActivity.java
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static org.androidtown.tablayoutexample.R.id.map;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -20,8 +22,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -37,6 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
